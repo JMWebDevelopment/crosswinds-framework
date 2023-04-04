5fa5e34b343cdd423396834bbde491832e7f37ba
Jacob Martella <me@jacobmartella.com>
2023-04-03 21:48:11 -0500

    Added in default widget pattern.
    Addresses #7

---
+++ patterns/widget-default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Widget - Default
+ * Slug: jm-web-dev-fse-starter-theme/widget-default
+ * Categories: jm-web-dev-fse-starter-theme-widgets
+ * Viewport Width: 370
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"10px","padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"border":{"width":"1px"}},"borderColor":"contrast","backgroundColor":"base","className":"widget widget-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group widget widget-default has-border-color has-contrast-border-color has-base-background-color has-background" style="border-width:1px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"10px","right":"10px","bottom":"10px","left":"10px"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px"><!-- wp:heading {"textAlign":"center","textColor":"background","fontSize":"medium"} -->
+<h2 class="has-text-align-center has-background-color has-text-color has-medium-font-size"><?php _e( 'Widget Title', 'crosswinds-framework' ); ?></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"10px","right":"10px","bottom":"10px","left":"10px"},"blockGap":"10px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px"><!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
