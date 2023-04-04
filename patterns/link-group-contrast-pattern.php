5c26bc62f52d0862921488bff510820de673ed7a
Jacob Martella <me@jacobmartella.com>
2023-04-04 09:30:13 -0500

    Added in the link group patterns.
    Addresses #8

---
+++ patterns/link-group-contrast-pattern.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Title: Link Group - Contrast
+ * Slug: jm-web-dev-fse-starter-theme/link-group-contrast-pattern
+ * Categories: jm-web-dev-fse-starter-theme-links
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"50px","right":"15px","bottom":"50px","left":"15px"}},"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"backgroundColor":"contrast","textColor":"base","className":"link-group-contrast-pattern","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull link-group-contrast-pattern has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:50px;padding-right:15px;padding-bottom:50px;padding-left:15px"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"align":"center","id":968,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px"}},"className":"is-style-default"} -->
+<figure class="wp-block-image aligncenter size-full has-custom-border is-style-default"><img src="http://wordpress-dev.local/crosswinds-framework/wp-content/uploads/sites/4/2013/03/image-alignment-150x150-1.jpg" alt="Image Alignment 150x150" class="wp-image-968" style="border-radius:100px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size"><?php _e( 'Your Name or Heading Here', 'jm-web-dev-fse-starter-theme' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php _e( 'WordPress Web Developer', 'jm-web-dev-fse-starter-theme' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 1', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 2', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 3', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 4', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 5', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 6', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 7', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Link 8', 'jm-web-dev-fse-starter-theme' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
