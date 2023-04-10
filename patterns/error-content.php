9c73c66710dc12328190e0f5272f1dd6b2db30ad
Jacob Martella <me@jacobmartella.com>
2023-04-10 12:57:24 -0500

    Started working on the 404 template.
    Addresses #19

---
+++ patterns/error-content.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Error Content
+ * Slug: jm-web-dev-fse-starter-theme/error-content
+ * Categories: jm-web-dev-fse-starter-theme-404
+ * Viewport Width: 800
+ */
+?>
+
+<!-- wp:paragraph -->
+<p><?php _e( 'Oops. It looks like the page you\'re looking for isn\'t here. You can use the search form below or look through the links to find the post or page you\'re looking for.', 'jm-web-dev-fse-starter-theme' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:search {"label":"<?php _e( 'Search', 'jm-web-dev-fse-starter-theme' ); ?>","buttonText":"<?php _e( 'Search', 'jm-web-dev-fse-starter-theme' ); ?>"} /-->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php _e( 'Pages', 'jm-web-dev-fse-starter-theme' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:page-list /-->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php _e( 'Categories', 'jm-web-dev-fse-starter-theme' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:categories /-->
---
+++ patterns/error-header-base-centered.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Error Header with Base Centered Title
+ * Slug: jm-web-dev-fse-starter-theme/error-header-base-centered
+ * Categories: jm-web-dev-fse-starter-theme-404
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-large","left":"var:preset|spacing|x-small"}}},"className":"page-header-base-centered","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull page-header-base-centered" style="padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"max-60"} -->
+<h1 class="wp-block-heading has-text-align-center has-max-60-font-size">Error 404 — Page Not Found</h1>
+<!-- /wp:heading -->
+<!-- wp:crosswinds-blocks/breadcrumbs {"align":"center","separatorIcon":"chevron-right","fontSize":"medium"} /--></div>
+<!-- /wp:group -->
