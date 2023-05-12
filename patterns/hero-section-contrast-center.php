336d0260f2c7278e6f7930267378bc38fb655899
Jacob Martella <me@jacobmartella.com>
2023-05-11 19:49:25 -0500

    Added in the new hero sections.

---
+++ patterns/hero-section-contrast-center.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Hero Section - Contrast with Centered Photo and Text
+ * Slug: crosswinds-framework/hero-section-contrast-center
+ * Categories: crosswinds-framework-hero-sections
+ * Viewport Width: 1200
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-large","left":"var:preset|spacing|x-small"},"blockGap":"var:preset|spacing|medium"}},"backgroundColor":"contrast","textColor":"base","className":"hero-section hero-section-contrast-center is-style-full-height","layout":{"type":"constrained","wideSize":"1200px","contentSize":"800px"}} -->
+<div class="wp-block-group alignfull hero-section hero-section-contrast-center is-style-full-height has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:heading {"textAlign":"center","fontSize":"max-72"} -->
+<h2 class="wp-block-heading has-text-align-center has-max-72-font-size"><?php _e( 'This is a Heading', 'crosswinds-framework' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"base","textColor":"contrast","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Add a Button Here', 'crosswinds-framework' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:image {"id":768,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/welcome-to-mansfield.jpg" alt="Huatulco Coastline" class="wp-image-768"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
---
+++ patterns/hero-section-cover-right.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Hero Section - Cover with Text Right
+ * Slug: crosswinds-framework/hero-section-cover-right
+ * Categories: crosswinds-framework-hero-sections
+ * Viewport Width: 1440
+ */
+?>
+
+<!-- wp:group {"align":"full","className":"hero-section hero-section-cover-right","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull hero-section hero-section-cover-right"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/welcome-to-mansfield.jpg","id":1891,"dimRatio":50,"minHeight":100,"minHeightUnit":"vh","align":"full"} -->
+<div class="wp-block-cover alignfull" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1891" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/welcome-to-mansfield.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","wideSize":"1440px"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|medium"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","fontSize":"max-90"} -->
+<h2 class="wp-block-heading has-text-align-center has-max-90-font-size"><?php _e( 'This is a Heading', 'crosswinds-framework' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"large"} -->
+<p class="has-text-align-left has-large-font-size" style="font-style:normal;font-weight:400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"base","textColor":"contrast","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button"><?php _e( 'Add a Button Here', 'crosswinds-framework' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
